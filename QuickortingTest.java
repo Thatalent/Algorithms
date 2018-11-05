451f1783f27f896c6597a8d77cdbab7484bfa019
Jonathan Hudson <jonathan.hudson2012@gmail.com>
2018-11-05 12:14:37 -0500

    Update QuickortingTest.java

--- QuickortingTest.java
+++ QuickortingTest.java
@@ -1,1 +1,15 @@
+public class QuickortingTest{
 
+    public static void main(String[] args){
+
+        System.out.println("Starting proper quick sorting funciton");
+        QuicksortInPlace.quickSort(new int[]{1,3,5,3,9,0,10,35,32,2,5,83,8,57,2,49}, 0,15);
+        System.out.println("End");
+
+        System.out.println("\nStarting improper quick sorting funciton");
+
+        QuickSortInPlaceFailure.quickSort(new int[]{1,3,5,3,9,0,10,35,32,2,5,83,8,57,2,49}, 100,15);
+        System.out.println("End");
+
+    }
+}
